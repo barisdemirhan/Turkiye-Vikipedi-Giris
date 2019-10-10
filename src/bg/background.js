461ac50e4cf0445c7a1767707d1bc3445bb4a9ae
Barış Demirhan <baris.demirhan@outlook.com>
2019-10-10 23:49:47 +0300

    versiyon 0.0.8 Hata giderildi

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,17 +1,17 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(d) {
-        var current_url = d.url;
-        var host = current_url.match(/(https?:\/\/|www\.?)(www\.)?([^\/\s]+)/ig)[0];
-        host = host.replace("wikipedia.org","wikipedi0.org");
-        var el = document.createElement('a');
-        el.href = current_url;
-        var path = el.pathname;
-        return {redirectUrl:host+path};
-    },{urls: ["*://wikipedia.org/*","*://www.wikipedia.org/*","*://*.wikipedia.org/*",],
-       types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
-      },["blocking"]);
+  function(d) {
+    var current_url = d.url;
+    var host = current_url.match(/(https?:\/\/|www\.?)(www\.)?([^\/\s]+)/ig)[0];
+    host = host.replace("wikipedia.org","wikipedi0.org");
+    var el = document.createElement('a');
+    el.href = current_url;
+    var path = el.pathname;
+    return {redirectUrl:host+path};
+  },{urls: ["*://wikipedia.org/*","*://www.wikipedia.org/*","*://*.wikipedia.org/*",],
+  types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "font", "object", "xmlhttprequest", "ping", "csp_report", "media", "websocket", "other"]
+},["blocking"]);
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.update({
-        url: "https://kslnk.xyz/wikipedi0org"
-    });
+  chrome.tabs.update({
+    url: "https://wikipedi0.org"
+  });
 });
